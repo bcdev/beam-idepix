072ba66552ee49b131c22bf0d27f8ef94ed5b808
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2010-08-23 10:57:53 +0200

    -rename project from mepix to idepix -restructure project layout into a single module project

---
+++ src/test/java/org/esa/beam/mepix/operators/BarometricPressureOpTest.java
@@ -0,0 +1,19 @@
+package org.esa.beam.mepix.operators;
+
+import com.bc.jnn.JnnNet;
+import junit.framework.TestCase;
+
+/**
+ * Tests for class {@link org.esa.beam.mepix.operators.ComputeChainOp}.
+ *
+ * @author Olaf Danne
+ * @version $Revision: 1.1 $ $Date: 2008-10-09 16:43:53 $
+ */
+public class BarometricPressureOpTest extends TestCase {
+	private JnnNet neuralNet;
+
+    public void testSomething() {
+    }
+
+   
+}
