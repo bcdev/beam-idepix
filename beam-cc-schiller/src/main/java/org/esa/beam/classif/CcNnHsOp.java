e5a5950294ee0f35fe74272696c264d1be273ae9
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2013-10-22 16:59:15 +0200

    Added todos for review

--- beam-cc-schiller/src/main/java/org/esa/beam/classif/CcNnHsOp.java
+++ beam-cc-schiller/src/main/java/org/esa/beam/classif/CcNnHsOp.java
@@ -8,33 +8,55 @@ import org.esa.beam.framework.gpf.OperatorSpi;
 import org.esa.beam.framework.gpf.annotations.OperatorMetadata;
 import org.esa.beam.framework.gpf.annotations.Parameter;
 import org.esa.beam.framework.gpf.annotations.SourceProduct;
-import org.esa.beam.framework.gpf.pointop.*;
+import org.esa.beam.framework.gpf.pointop.PixelOperator;
+import org.esa.beam.framework.gpf.pointop.ProductConfigurer;
+import org.esa.beam.framework.gpf.pointop.Sample;
+import org.esa.beam.framework.gpf.pointop.SampleConfigurer;
+import org.esa.beam.framework.gpf.pointop.WritableSample;
 import org.esa.beam.jai.ResolutionLevel;
 import org.esa.beam.jai.VirtualBandOpImage;
 
-import java.awt.*;
+import java.awt.Rectangle;
+
+// todo nf - discuss operator action
+// todo nf - discuss unit-level testing of metadata values
 
 @OperatorMetadata(alias = "Meris.CCNNHS",
-        version = "2.0.2",
-        authors = "Helmut Schiller (Algorithm), Tom Block (Implementation)",
-        copyright = "(c) 2013 by Brockmann Consult",
-        description = "Computing cloud masks using neural networks by H.Schiller")
+                  version = "2.0.2",
+                  authors = "H. Schiller (Algorithm), T. Block, N. Fomferra (Implementation)",
+                  copyright = "(c) 2013 by Brockmann Consult",
+                  description = "Computing cloud masks using neural networks by H.Schiller")
 public class CcNnHsOp extends PixelOperator {
+
+    public static final String ALGORITHM_2013_03_01 = AlgorithmFactory.ALGORITHM_2013_03_01;
+    public static final String ALGORITHM_2013_05_09 = AlgorithmFactory.ALGORITHM_2013_05_09;
 
     @SourceProduct
     private Product sourceProduct;
 
     @Parameter(defaultValue = "NOT l1_flags.INVALID",
-            description = "A flag expression that defines pixels to be processed.")
+               description = "A flag expression that defines pixels to be processed.")
     private String validPixelExpression;
 
-    @Parameter(defaultValue = AlgorithmFactory.ALGORITHM_2013_03_01,
-            valueSet = {AlgorithmFactory.ALGORITHM_2013_03_01,
-                    AlgorithmFactory.ALGORITHM_2013_05_09},
-            description = "Select algorithm for processing.")
+    @Parameter(defaultValue = ALGORITHM_2013_03_01,
+               valueSet = {ALGORITHM_2013_03_01, ALGORITHM_2013_05_09},
+               description = "Select algorithm for processing.")
     private String algorithmName;
 
+    // todo nf - Introduce this parameter (not easily done in current design)
+    // @Parameter(defaultValue = "true", description = "Whether to output TOA reflectances")
+    // private boolean outputToaReflectances;
+
+    // todo nf - discuss faster and easier ways to access mask values
     private VirtualBandOpImage validOpImage;
+
+    public void setValidPixelExpression(String validPixelExpression) {
+        this.validPixelExpression = validPixelExpression;
+    }
+
+    public void setAlgorithmName(String algorithmName) {
+        this.algorithmName = algorithmName;
+    }
 
     private ThreadLocal<Rectangle> sampleRegion = new ThreadLocal<Rectangle>() {
         @Override
@@ -60,9 +82,10 @@ public class CcNnHsOp extends PixelOperator {
         final CCAlgorithm ccAlgorithm = AlgorithmFactory.get(algorithmName);
         ccAlgorithm.prepareInputs(sourceProduct);
 
+        // todo nf - must dispose this!
         validOpImage = VirtualBandOpImage.createMask(validPixelExpression,
-                sourceProduct,
-                ResolutionLevel.MAXRES);
+                                                     sourceProduct,
+                                                     ResolutionLevel.MAXRES);
     }
 
     @Override
@@ -86,6 +109,7 @@ public class CcNnHsOp extends PixelOperator {
     private boolean isSampleValid(int x, int y) {
         final Rectangle localRect = sampleRegion.get();
         localRect.setLocation(x, y);
+        // todo nf - optimize me: this is a very slow pixel access here
         return validOpImage.getData(localRect).getSample(x, y, 0) != 0;
     }
 
