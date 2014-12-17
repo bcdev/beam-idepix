a07c25e381648c1094972f5b5e72ccf1e037cf35
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2014-12-17 10:29:19 +0100

    use the "right" junit Assert class

--- beam-idepix-core/src/test/java/org/esa/beam/idepix/util/BresenhamTest.java
+++ beam-idepix-core/src/test/java/org/esa/beam/idepix/util/BresenhamTest.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 import java.awt.*;
 import java.util.List;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class BresenhamTest {
 
