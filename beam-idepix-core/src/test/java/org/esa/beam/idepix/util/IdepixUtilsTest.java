d9d7819a301d063df7687645877d0c2a3b030628
marpet <marco.peters@brockmann-consult.de>
2015-09-08 16:44:18 +0200

    turned test into Junit4 test

--- beam-idepix-core/src/test/java/org/esa/beam/idepix/util/IdepixUtilsTest.java
+++ beam-idepix-core/src/test/java/org/esa/beam/idepix/util/IdepixUtilsTest.java
@@ -1,29 +1,35 @@
 package org.esa.beam.idepix.util;
 
-import junit.framework.TestCase;
 import org.esa.beam.framework.datamodel.Band;
 import org.esa.beam.framework.datamodel.MetadataElement;
 import org.esa.beam.framework.datamodel.Product;
 import org.esa.beam.framework.datamodel.ProductData;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests for class {@link org.esa.beam.idepix.util.IdepixUtils}.
  *
  * @author Olaf Danne
- * @version $Revision: 1.1 $ $Date: 2008-10-09 16:43:53 $
  */
-public class IdepixUtilsTest extends TestCase {
+public class IdepixUtilsTest {
 
+    @Test
     public void testCorrectSaturatedReflectances() {
         float[] reflOrig = new float[]{Float.NaN, Float.NaN, Float.NaN, 12.3f};
 
         float[] reflCorr = IdepixUtils.correctSaturatedReflectances(reflOrig);
         assertNotNull(reflCorr);
         assertEquals(4, reflCorr.length);
-        assertEquals(12.3f, reflCorr[3]);
-        assertEquals(12.3f, reflCorr[2]);
-        assertEquals(12.3f, reflCorr[1]);
-        assertEquals(12.3f, reflCorr[0]);
+        assertEquals(12.3f, reflCorr[3], 1.0e-6f);
+        assertEquals(12.3f, reflCorr[2], 1.0e-6f);
+        assertEquals(12.3f, reflCorr[1], 1.0e-6f);
+        assertEquals(12.3f, reflCorr[0], 1.0e-6f);
 
         reflOrig = new float[]{Float.NaN, Float.NaN, Float.NaN, Float.NaN};
         reflCorr = IdepixUtils.correctSaturatedReflectances(reflOrig);
@@ -34,40 +40,41 @@ public class IdepixUtilsTest extends TestCase {
 
         reflOrig = new float[]{9.2f, 10.3f, 11.4f, 12.5f};
         reflCorr = IdepixUtils.correctSaturatedReflectances(reflOrig);
-        assertEquals(12.5f, reflCorr[3]);
-        assertEquals(11.4f, reflCorr[2]);
-        assertEquals(10.3f, reflCorr[1]);
-        assertEquals(9.2f, reflCorr[0]);
+        assertEquals(12.5f, reflCorr[3], 1.0e-6f);
+        assertEquals(11.4f, reflCorr[2], 1.0e-6f);
+        assertEquals(10.3f, reflCorr[1], 1.0e-6f);
+        assertEquals(9.2f, reflCorr[0], 1.0e-6f);
 
         reflOrig = new float[]{9.2f, Float.NaN, Float.NaN, 12.3f};
         reflCorr = IdepixUtils.correctSaturatedReflectances(reflOrig);
-        assertEquals(12.3f, reflCorr[3]);
-        assertEquals(12.3f, reflCorr[2]);
-        assertEquals(12.3f, reflCorr[1]);
-        assertEquals(9.2f, reflCorr[0]);
+        assertEquals(12.3f, reflCorr[3], 1.0e-6f);
+        assertEquals(12.3f, reflCorr[2], 1.0e-6f);
+        assertEquals(12.3f, reflCorr[1], 1.0e-6f);
+        assertEquals(9.2f, reflCorr[0], 1.0e-6f);
 
         reflOrig = new float[]{9.2f, Float.NaN, Float.NaN, Float.NaN};
         reflCorr = IdepixUtils.correctSaturatedReflectances(reflOrig);
-        assertEquals(9.2f, reflCorr[3]);
-        assertEquals(9.2f, reflCorr[2]);
-        assertEquals(9.2f, reflCorr[1]);
-        assertEquals(9.2f, reflCorr[0]);
+        assertEquals(9.2f, reflCorr[3], 1.0e-6f);
+        assertEquals(9.2f, reflCorr[2], 1.0e-6f);
+        assertEquals(9.2f, reflCorr[1], 1.0e-6f);
+        assertEquals(9.2f, reflCorr[0], 1.0e-6f);
 
         reflOrig = new float[]{9.2f, Float.NaN, 12.3f, Float.NaN};
         reflCorr = IdepixUtils.correctSaturatedReflectances(reflOrig);
-        assertEquals(12.3f, reflCorr[3]);
-        assertEquals(12.3f, reflCorr[2]);
-        assertEquals(12.3f, reflCorr[1]);
-        assertEquals(9.2f, reflCorr[0]);
+        assertEquals(12.3f, reflCorr[3], 1.0e-6f);
+        assertEquals(12.3f, reflCorr[2], 1.0e-6f);
+        assertEquals(12.3f, reflCorr[1], 1.0e-6f);
+        assertEquals(9.2f, reflCorr[0], 1.0e-6f);
 
         reflOrig = new float[]{Float.NaN, 9.2f, Float.NaN, 12.3f};
         reflCorr = IdepixUtils.correctSaturatedReflectances(reflOrig);
-        assertEquals(12.3f, reflCorr[3]);
-        assertEquals(12.3f, reflCorr[2]);
-        assertEquals(9.2f, reflCorr[1]);
-        assertEquals(9.2f, reflCorr[0]);
+        assertEquals(12.3f, reflCorr[3], 1.0e-6f);
+        assertEquals(12.3f, reflCorr[2], 1.0e-6f);
+        assertEquals(9.2f, reflCorr[1], 1.0e-6f);
+        assertEquals(9.2f, reflCorr[0], 1.0e-6f);
     }
 
+    @Test
     public void testAreAllReflectancesValid() {
         float[] reflOrig = new float[]{12.3f, 12.3f, 12.3f, 12.3f};
         assertTrue(IdepixUtils.areAllReflectancesValid(reflOrig));
@@ -76,6 +83,7 @@ public class IdepixUtilsTest extends TestCase {
         assertFalse(IdepixUtils.areAllReflectancesValid(reflOrig));
     }
 
+    @Test
     public void testIsNoReflectanceValid() {
         float[] reflOrig = new float[]{Float.NaN, Float.NaN, Float.NaN, 12.3f};
         assertFalse(IdepixUtils.isNoReflectanceValid(reflOrig));
@@ -84,18 +92,19 @@ public class IdepixUtilsTest extends TestCase {
         assertTrue(IdepixUtils.isNoReflectanceValid(reflOrig));
     }
 
+    @Test
     public void testSpectralSlope() {
         float wvl1 = 450.0f;
         float wvl2 = 460.0f;
         float refl1 = 50.0f;
         float refl2 = 100.0f;
-        assertEquals(5.0f, IdepixUtils.spectralSlope(refl1, refl2, wvl1, wvl2));
+        assertEquals(5.0f, IdepixUtils.spectralSlope(refl1, refl2, wvl1, wvl2), 1.0e-6f);
 
         wvl1 = 450.0f;
         wvl2 = 460.0f;
         refl1 = 500.0f;
         refl2 = 100.0f;
-        assertEquals(-40.0f, IdepixUtils.spectralSlope(refl1, refl2, wvl1, wvl2));
+        assertEquals(-40.0f, IdepixUtils.spectralSlope(refl1, refl2, wvl1, wvl2), 1.0e-6f);
 
         wvl1 = 450.0f;
         wvl2 = 450.0f;
@@ -105,22 +114,24 @@ public class IdepixUtilsTest extends TestCase {
         assertTrue(Float.isInfinite(slope));
     }
 
+    @Test
     public void testSetNewBandProperties() {
         Band band1 = new Band("test", ProductData.TYPE_FLOAT32, 10, 10);
         IdepixUtils.setNewBandProperties(band1, "bla", "km", -999.0, false);
         assertEquals("bla", band1.getDescription());
         assertEquals("km", band1.getUnit());
-        assertEquals(-999.0, band1.getNoDataValue());
+        assertEquals(-999.0, band1.getNoDataValue(), 1.0e-8);
         assertEquals(false, band1.isNoDataValueUsed());
 
         Band band2 = new Band("test2", ProductData.TYPE_INT32, 10, 10);
         IdepixUtils.setNewBandProperties(band2, "blubb", "ton", -1, true);
         assertEquals("blubb", band2.getDescription());
         assertEquals("ton", band2.getUnit());
-        assertEquals(-1.0, band2.getNoDataValue());
+        assertEquals(-1.0, band2.getNoDataValue(), 1.0e-8);
         assertEquals(true, band2.isNoDataValueUsed());
     }
 
+    @Test
     public void testConvertGeophysicalToMathematicalAngle() {
         double geoAngle = IdepixUtils.convertGeophysicalToMathematicalAngle(31.0);
         assertEquals(59.0, geoAngle, 1.0);
@@ -134,6 +145,7 @@ public class IdepixUtilsTest extends TestCase {
         assertTrue(Double.isNaN(geoAngle));
     }
 
+    @Test
     public void testIsAvhrrTimelineProduct() {
         final Product product = new Product("tl", "tl", 1, 1);
         final MetadataElement globalAttrElem = new MetadataElement("Global_Attributes");
@@ -145,6 +157,7 @@ public class IdepixUtilsTest extends TestCase {
         assertFalse(IdepixUtils.isAvhrrTimelineProduct(product));
     }
 
+    @Test
     public void testGetAvhrrTimelineNoaaId() {
         final Product product = new Product("tl", "tl", 1, 1);
         final MetadataElement globalAttrElem = new MetadataElement("Global_Attributes");
@@ -153,13 +166,13 @@ public class IdepixUtilsTest extends TestCase {
         product.getMetadataRoot().addElement(globalAttrElem);
         String noaaId = IdepixUtils.getAvhrrTimelineNoaaId(product);
         assertNotNull(noaaId);
-        assertTrue(noaaId.equals("14"));
+        assertEquals("14", noaaId);
 
         globalAttrElem.setAttributeString("platform", "NOAA_8");
         product.getMetadataRoot().addElement(globalAttrElem);
         noaaId = IdepixUtils.getAvhrrTimelineNoaaId(product);
         assertNotNull(noaaId);
-        assertTrue(noaaId.equals("8"));
+        assertEquals("8", noaaId);
 
         globalAttrElem.setAttributeString("platform", "blabla");
         product.getMetadataRoot().addElement(globalAttrElem);
